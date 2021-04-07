94879ba38fc753f4dc367cb85957ce1e26ec782b
KnightCode <mail2sagarsalunkhe@gmail.com>
2021-04-08 01:26:52 +0530

    Moved Code Related To Tree Traversal to TreeTraversal.cs File

--- Binary Tree DS and Basic Operations/Program.cs
+++ Binary Tree DS and Basic Operations/Program.cs
@@ -8,7 +8,14 @@ namespace Binary_Tree_DS_and_Basic_Operations
         static void Main(string[] args)
         {
             root = new BTNode(1);
+            CreateHardCodedBinaryTree(root);
+            TreeTraversals.InOrderRec(root);
+        }
 
+        private static void CreateHardCodedBinaryTree(BTNode root)
+        {
+            root.LChild = new BTNode(2);
+            root.RChild = new BTNode(3);
         }
     }
 }
---
+++ Binary Tree DS and Basic Operations/TreeTraversals.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Tree_DS_and_Basic_Operations
+{
+    public static class TreeTraversals
+    {
+        public static void InOrderRec(BTNode root)
+        {
+            if (root != null)
+            {
+                InOrderRec(root.LChild);
+                Console.WriteLine(root.Data);
+                InOrderRec(root.RChild);
+            }
+        }
+        public static void PreOrderRec()
+        {
+
+        }
+        public static void LevelOrderRec()
+        {
+
+        }
+        public static void PostOrderRec()
+        {
+
+        }
+        public static void InOrder()
+        {
+
+        }
+        public static void PreOrder()
+        {
+
+        }
+        public static void PostOrder()
+        {
+
+        }
+        public static void LevelOrder()
+        {
+
+        }
+    }
+}
