52eb90a649d541fd0208ba83524b1fc0b86a48d0
KnightCode <mail2sagarsalunkhe@gmail.com>
2021-04-08 00:49:36 +0530

    Added Constructor To BTNode Class to accept data

--- Binary Tree DS and Basic Operations/BTNode.cs
+++ Binary Tree DS and Basic Operations/BTNode.cs
@@ -9,5 +9,10 @@ namespace Binary_Tree_DS_and_Basic_Operations
         public BTNode LChild { get; set; }
         public int Data { get; set; }
         public BTNode RChild { get; set; }
+
+        public BTNode(int data)
+        {
+
+        }
     }
 }
