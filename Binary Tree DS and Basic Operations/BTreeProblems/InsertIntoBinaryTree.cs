66279ccf5b9befe6875bc22e9c24cbf11c750333
KnightCode <mail2sagarsalunkhe@gmail.com>
2021-04-18 22:21:17 +0530

    Inserting Node into Binary Tree

---
+++ Binary Tree DS and Basic Operations/BTreeProblems/InsertIntoBinaryTree.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Tree_DS_and_Basic_Operations.BTreeProblems
+{
+    /// <summary>
+    /// Level order traversal to insert node which has LChild or RChild null 
+    /// </summary>
+    public class InsertIntoBinaryTree
+    {
+        public static void InsertNode(BTNode root, int data)
+        {
+            if (root == null)
+            {
+                root = new BTNode(data);
+                return;
+            }
+            else
+            {
+                Queue<BTNode> btNodeQueue = new Queue<BTNode>();
+                btNodeQueue.Enqueue(root);
+                while (btNodeQueue.Count > 0)
+                {
+                    var temp = btNodeQueue.Dequeue();
+                    if (temp.LChild != null)
+                        btNodeQueue.Enqueue(temp.LChild);
+                    else
+                    {
+                        temp.LChild = new BTNode(data);
+                        return;
+                    }
+                    if (temp.RChild != null)
+                        btNodeQueue.Enqueue(temp.RChild);
+                    else
+                    {
+                        temp.RChild = new BTNode(data);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
